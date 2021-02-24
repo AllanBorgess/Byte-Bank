ed8f0caa3420b6362224637e1364a63c3338ae01
Allan <allanborges20000@gmail.com>
2021-02-24 19:06:59 -0300

    Byte-Bank

---
+++ bytebank-herdado/src/ContaCorrente.java
@@ -0,0 +1,23 @@
+
+public class ContaCorrente extends Conta implements Tributavel{
+	
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia,numero);
+	}
+	public double getValorImposto() {
+		return super.saldo * 0.01;
+	}
+	
+	@Override
+	public void saca(double valor) throws SaldoInsuficienteException {
+		double ValorASacar = valor + 0.2;
+		 super.saca(ValorASacar);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+		
+	}
+	
+}
